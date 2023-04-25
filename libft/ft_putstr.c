3d9597f593c140f90dad45d8dc48f8af76bf912a
Shin Kim <044apde@gmail.com>
2023-04-25 11:48:36 +0900

    Fix ft_putstr

--- libft/ft_putstr.c
+++ libft/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: shikim <shikim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 07:08:00 by shikim            #+#    #+#             */
-/*   Updated: 2023/04/25 08:19:00 by shikim           ###   ########.fr       */
+/*   Updated: 2023/04/25 11:47:54 by shikim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ int	ft_putstr(char *s)
 	int	len;
 
 	len = ft_strlen(s);
+	if (s == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	while (*s != '\0')
 		write(1, s++, 1);
 	return (len);
